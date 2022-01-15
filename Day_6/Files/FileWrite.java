0c0fbb7c456d895a9c33e1e07d6b9a6808a74f2c
ASaleh <95314680+AmSaleh21@users.noreply.github.com>
2022-01-15 22:01:24 +0200

    Update FileWrite.java

--- Day_6/Files/FileWrite.java
+++ Day_6/Files/FileWrite.java
@@ -6,16 +6,16 @@ public class FileWrite {
         PrintWriter pwr = null;
         
         try {
-            fwr = new FileWriter("sample.txt", true);
-            pwr = new PrintWriter(fwr);
+            fwr = new FileWriter("sample.txt", true);  //filewriter constructor to open the file - true flag is to append don't re-write
+            pwr = new PrintWriter(fwr); 
             pwr.println();
-            for(String data : args){
+            for(String data : args){ //append the args data in the file
                 pwr.println(data);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }finally{
-            try {
+            try { //try to close the file
                 pwr.close();
                 fwr.close();
             } catch (Exception e) {
